4bb00f2df3fcf169564f9a2486c70ad6b50d3f2e
Nazarova Elena <nazzarova.in@gmail.com>
2024-11-06 12:21:22 +0200

    Logic for adding a like to a post

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return self.title
+
+    def total_likes(self):
+        return self.likes.all().count()
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializers
@@ -11,3 +13,22 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+
+        if user in post.likes.all():
+            post.likes.remove(user)
+            liked = False
+
+        else:
+            post.likes.add(user)
+            liked = True
+
+        post.save()
+
+        serializer = self.get_serializer(post)
+        return Response({'liked': liked, 'post': serializer.data}, status=status.HTTP_200_OK)
+
